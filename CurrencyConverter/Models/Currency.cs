29e038cfd513ec3bebce9a2499cd62ce076e77c7
Никита Скрынник <PM74.Skrynnik@outlook.com>
2021-09-17 20:29:54 +0700

    Delete unnecessary fields in Currency class

--- CurrencyConverter/Models/Currency.cs
+++ CurrencyConverter/Models/Currency.cs
@@ -2,12 +2,9 @@
 {
     public class Currency
     {
-        public string ID { get; set; }
-        public string NumCode { get; set; }
         public string CharCode { get; set; }
         public int Nominal { get; set; }
         public string Name { get; set; }
         public double Value { get; set; }
-        public decimal Previous { get; set; }
     }
 }
