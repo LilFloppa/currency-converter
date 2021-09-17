256ce8b79b1d09ef71699191aa83cdaae654158d
Никита Скрынник <PM74.Skrynnik@outlook.com>
2021-09-17 21:40:34 +0700

    Add CurrencyToString Converter

---
+++ CurrencyConverter/Converters/CurrencyToStringConverter.cs
@@ -0,0 +1,23 @@
+﻿using CurrencyConverter.Models;
+using System;
+using Windows.UI.Xaml.Data;
+
+namespace CurrencyConverter.Converters
+{
+    public class CurrencyToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null)
+                return "";
+
+            Currency currency = (Currency)value;
+            return currency.CharCode;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
