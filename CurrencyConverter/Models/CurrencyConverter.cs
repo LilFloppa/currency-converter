6f040c8e77c7566f66d47f5259bdb2ecfac57b0d
Никита Скрынник <PM74.Skrynnik@outlook.com>
2021-09-17 20:29:32 +0700

    Add PropertyChanged events in MainViewModel

---
+++ CurrencyConverter/Models/CurrencyConverter.cs
@@ -0,0 +1,10 @@
+﻿namespace CurrencyConverter.Models
+{
+    public static class CurrencyConverter
+    {
+        public static double Convert(Currency fromCurrency, Currency toCurrency, double value)
+        {
+            return value;
+        }
+    }
+}
--- CurrencyConverter/ViewModels/MainViewModel.cs
+++ CurrencyConverter/ViewModels/MainViewModel.cs
@@ -1,19 +1,67 @@
 ﻿using CurrencyConverter.Models;
 using CurrencyConverter.Services;
+using ReactiveUI;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Threading.Tasks;
 
 namespace CurrencyConverter.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
         private readonly ICurrencyService currencyService;
+
         public List<Currency> CurrencyList { get; set; }
 
-        public Currency FromCurrency { get; set; }
-        public Currency ToCurrency { get; set; }
-        public double FromValue { get; set; }
-        public double ToValue { get; set; }
+        public Currency FromCurrency 
+        {
+            get => fromCurrency;
+            set
+            {
+                fromCurrency = value;
+                OnPropertyChanged("FromCurrency");
+            }
+        }
+
+        private Currency fromCurrency;
+        public Currency ToCurrency
+        {
+            get => toCurrency;
+            set
+            {
+                toCurrency = value;
+                OnPropertyChanged("ToCurrency");
+            }
+        }
+
+        private Currency toCurrency;
+
+        public double FromValue 
+        {
+            get => fromValue;
+            set
+            {
+                fromValue = value;
+                OnPropertyChanged("FromValue");
+            }
+        }
+
+        private double fromValue = 0.0;
+        public double ToValue
+        {
+            get => toValue;
+            set
+            {
+                toValue = value;
+                OnPropertyChanged("ToValue");
+            }
+        }
+
+        private double toValue = 0.0;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public MainViewModel(ICurrencyService currencyService)
         {
             this.currencyService = currencyService;
@@ -23,6 +71,25 @@ namespace CurrencyConverter.ViewModels
         private async Task Intiailize()
         {
             CurrencyList = await currencyService.GetCurrencyListAsync();
+
+            Currency rub = new Currency
+            {
+                CharCode = "RUB",
+                Name = "Рубль",
+                Value = 1.0,
+                Nominal = 1
+            };
+
+            CurrencyList.Add(rub);
+            FromCurrency = rub;
+            ToCurrency = CurrencyList.Find(cur => cur.CharCode == "USD");
+        }
+
+        private void Convert() => ToValue = Models.CurrencyConverter.Convert(fromCurrency, toCurrency, fromValue);
+
+        private void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
