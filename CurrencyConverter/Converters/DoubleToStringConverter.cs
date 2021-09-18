dc5e02453330d29754a212f2bca402af28a27cc4
Никита Скрынник <PM74.Skrynnik@outlook.com>
2021-09-18 17:08:40 +0700

    fix double parsing (didn't work with dot) + fix button styles

---
+++ CurrencyConverter/Converters/DoubleToStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using Windows.UI.Xaml.Data;
+
+namespace CurrencyConverter.Converters
+{
+    class DoubleToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            double doubleValue = (double)value;
+
+            return doubleValue.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            string stringValue = (string)value;
+
+            stringValue = stringValue.Replace('.', ',');
+
+            return double.Parse(stringValue, NumberStyles.Any);
+        }
+    }
+}
