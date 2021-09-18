2486336b0eb2ac823e7dcb2963589308e84aab15
Никита Скрынник <PM74.Skrynnik@outlook.com>
2021-09-18 16:11:54 +0700

    Add DoubleTextInput (for doubles only)

---
+++ CurrencyConverter/UserControls/DoubleTextBox.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Windows.UI.Xaml.Controls;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace CurrencyConverter.UserControls
+{
+    public sealed class DoubleTextBox : TextBox
+    {
+        public DoubleTextBox()
+        {
+            BeforeTextChanging += OnBeforeTextChanging;
+        }
+
+        private void OnBeforeTextChanging(TextBox sender, TextBoxBeforeTextChangingEventArgs args)
+        {
+            args.Cancel = 
+                args.NewText.Any(c => !char.IsDigit(c) && c != '.' && c != ',') || 
+                args.NewText.Count(c => c == '.' || c == ',') > 1;
+        }
+    }
+}
